dfdf0115498e4267d4df1286d62559acd0627390
Austin Bullard <abullard@leantechniques.com>
2024-03-10 00:09:12 -0600

    prettier added

---
+++ components/index/banner.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import { Photo } from './photo';
+
+export const Banner = () => {
+    return (
+        <div>
+            <Photo />
+        </div>
+    );
+};
+
+// const Circle = styled.img`
+//   border-radius: 60px 60px 60px 15px;
+//   height: 15rem;
+//   width: 15rem;
+// `;
